e17afb3d67138f3de1c8390f4a9d589accbb53dd
Sercan LÜLLECİ <56175274+srcnllc@users.noreply.github.com>
2022-12-01 12:37:06 +0300

    ProductDetail stillendirilmesi ve düzenlenmesi tamamlandı.

--- src/pages/ProductDetail/ProductDetail.js
+++ src/pages/ProductDetail/ProductDetail.js
@@ -1,37 +1,46 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from "react-router-dom";
+import Kargo from '../../components/Kargo/Kargo';
 import productdata from '../../json/data.json'
 import './ProductDetail.css'
 
 
 function ProductDetail() {
     let { name } = useParams("");
-    const [detailData,setDetailData] = useState([])
+    const [detailData, setDetailData] = useState([])
 
     useEffect(() => {
-        setDetailData(productdata.filter(item => item.name ===`${name}`))
-    },[name])
+        setDetailData(productdata.filter(item => item.name === `${name}`))
+    }, [name])
 
-console.log(detailData)
-  return (
-    <div className='detailpage'>
-            <div className='leftİmage'>
-        {detailData.map((item, index) => {
-          return (
-            <img src={item.img} alt={item.name} key={index}/>
-          )
-        })}
-    </div>
-        <div className='rightDesc'>
-        {detailData.map((item, index) => {
-          return (
-            <p key={index}>{item.desc}</p>
-          )
-        })}
-    </div>
-    </div>
-
-  )
+    console.log(detailData)
+    return (
+        <>
+            <div className='detailpage'>
+                <div className='leftPage'>
+                    {detailData.map((item, index) => {
+                        return (
+                            <img src={item.img} alt={item.name} key={index} />
+                        )
+                    })}
+                </div>
+                <div className='rightPage'>
+                    {detailData.map((item, index) => {
+                        return (
+                            <div key={index} className={'rightdesc'}>
+                                <h1 className='righttitle'>{item.name}</h1>
+                                <p>{item.desc}</p>
+                                <div className='priceButton'>
+                                    <h1>{`${item.price} ₺`}</h1>
+                                    <button onClick={() => { alert('tıkladnı') }}>SEPETE EKLE</button>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
+            <Kargo />
+        </>)
 }
 
 export default ProductDetail
